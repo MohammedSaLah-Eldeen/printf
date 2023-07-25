dd3c7dde07748d804dc6ef856eaa9852ce8517b4
Mohammed Salah <MohammedSalah.SuperM159@gmail.com>
2023-07-25 09:28:58 +0000

    mandatory tasks

---
+++ main.h
@@ -0,0 +1,15 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+#include <strarg.h>
+#include <unistd.h>
+#include <string.h>
+
+
+int print_char(char c);
+int print_string(char *c);
+int print_number(int num);
+int _printf(const char *format, ...);
+
+
+#endif /*_MAIN_H_*/
